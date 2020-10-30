e5a8c3a83e5550b90d695187b62e08d9850d475e
Hidetaka Okamoto <info@wp-kyoto.net>
2020-10-30 18:13:10 +0900

    try: nextjs with ionic

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+      deviceSizes: [320, 420, 768, 1024, 1200],
+      iconSizes: [],
+      domains: ['wp-kyoto.cdn.rabify.me'],
+      path: '/_next/image',
+      loader: 'default',
+    },
+  }
